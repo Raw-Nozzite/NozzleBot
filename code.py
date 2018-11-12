e64b0e2031e42204fa74dbdb9d91e7ae40d41d21
Raw-Nozzite <44949375+Raw-Nozzite@users.noreply.github.com>
2018-11-12 16:55:19 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,19 @@
 import discord.ext.commands as discord
+import random
 
 with open("discord-token.txt",mode="r") as f:
     token = f.readlines()[0]
 
 @command()
 async def ping():
-    bot.say("Pong")
+    botSpeech = random.randint(1,2)
+    if botSpeech == 1:
+        bot.say("Pong")
+    else:
+        bot.Say("I don't play table tennis")
 bot = discord.Bot()
 async def on_ready():
     print("Logged in as:")
     print(bot.user.name)
     print(bot.user.id)
     print("~"*50)
-asdasdasdasd
