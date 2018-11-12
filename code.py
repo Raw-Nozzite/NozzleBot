0e6cea8900c0e5014b50b70e172d174890231ce5
ZomBMage <ZomBMage@users.noreply.github.com>
2018-11-12 16:47:19 +0000

    Add initial commands

--- code.py
+++ code.py
@@ -1,5 +1,15 @@
-import discord.ext.commands
+import discord.ext.commands as discord
 
 with open("discord-token.txt",mode="r") as f:
     token = f.readlines()[0]
 
+@command()
+async def ping():
+    bot.say("Pong")
+bot = discord.Bot()
+async def on_ready():
+    print("Logged in as:")
+    print(bot.user.name)
+    print(bot.user.id)
+    print("~"*50)
+asdasdasdasd
