df27a4248fcc17f2bf0ed4331c9ab02f51f7904a
Raw-Nozzite <Raw-Nozzite@users.noreply.github.com>
2018-11-13 16:06:25 +0000

    Tried to add a blacklist function that doesn't work

--- code.py
+++ code.py
@@ -1,31 +1,43 @@
 import discord.ext.commands as discord
 import random    #Literally only used once
 import asyncio
+blisted = []
 
 with open("discord-token.txt",mode="r") as f:
     token = f.readlines()[0]
 bot = discord.Bot("..")
 
-@bot.command()                            #Test command for bot responsiveness
+@bot.command()                            
 async def ping(ctx):
+    """Test command for bot responsiveness"""
     botSpeech = random.randint(1,2)
     if botSpeech == 1:
         await ctx.channel.send("Pong!")
     else:
         await ctx.channel.send("I don't play table tennis. I bet you watched the anime.")
 
-@bot.command()                           #Instantly deletes the channel the command is
-async def lynch_channel(ctx):            #triggered in
-    await ctx.channel.delete()
+@bot.command()                           
+async def lynch_channel(ctx, args):    
+    """Instantly deletes the channel the command is
+triggered in"""
+    await ctx.args.delete()
 
-@bot.command()                           #Will eventually print the user's balance
+@bot.command()                           
 async def bal(ctx):
+    """Will eventually print the user's balance"""
     await ctx.channel.send("0")
     
-@bot.command()                           #Print the devs of the bot (Hard coded fight me Bax)
+@bot.command()                    
 async def devs(ctx):
+    """Print the devs of the bot (Hard coded fight me Bax"""
     await ctx.channel.send("""- Raw-Nozzite for Design and Programming
-- ZomBMage for BETTER programming""")
+- ZomBMage for BETTER Programming""")
+
+@bot.command()
+async def blist(ctx, userID):
+    """Used for stopping users from posting messages"""
+    blisted += userID
+    print(blisted)
 
 @bot.event
 async def on_ready():
