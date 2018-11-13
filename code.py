67f2997da17fc19601ef207f511233bf967e22bd
Raw-Nozzite <Raw-Nozzite@users.noreply.github.com>
2018-11-13 10:04:46 +0000

    Added comments and devs command

--- code.py
+++ code.py
@@ -1,28 +1,37 @@
 import discord.ext.commands as discord
-import random
+import random    #Literally only used once
 import asyncio
 
 with open("discord-token.txt",mode="r") as f:
     token = f.readlines()[0]
 bot = discord.Bot("..")
 
-@bot.command()
+@bot.command()                            #Test command for bot responsiveness
 async def ping(ctx):
     botSpeech = random.randint(1,2)
     if botSpeech == 1:
         await ctx.channel.send("Pong!")
     else:
         await ctx.channel.send("I don't play table tennis. I bet you watched the anime.")
-@bot.command()
-async def lynch_channel(ctx):
+
+@bot.command()                           #Instantly deletes the channel the command is
+async def lynch_channel(ctx):            #triggered in
     await ctx.channel.delete()
+
+@bot.command()                           #Will eventually print the user's balance
+async def bal(ctx):
+    await ctx.channel.send("0")
+    
+@bot.command()                           #Print the devs of the bot (Hard coded fight me Bax)
+async def devs(ctx):
+    await ctx.channel.send("""- Raw-Nozzite for Design and Programming
+- ZomBMage for BETTER programming""")
+
 @bot.event
 async def on_ready():
     print("Logged in as:")
     print(bot.user.name)
     print(bot.user.id)
     print("~"*50)
-    for channel in bot.get_all_channels():
-        if channel.name == "asdkj":
-            await channel.delete()
+
 bot.run(token)
