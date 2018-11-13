6bd41206cde74ab9e4ddcaad16f994aa0386c77c
ZomBMage <ZomBMage@users.noreply.github.com>
2018-11-13 09:10:25 +0000

    fun

--- code.py
+++ code.py
@@ -13,10 +13,16 @@ async def ping(ctx):
         await ctx.channel.send("Pong!")
     else:
         await ctx.channel.send("I don't play table tennis. I bet you watched the anime.")
+@bot.command()
+async def lynch_channel(ctx):
+    await ctx.channel.delete()
 @bot.event
 async def on_ready():
     print("Logged in as:")
     print(bot.user.name)
     print(bot.user.id)
     print("~"*50)
+    for channel in bot.get_all_channels():
+        if channel.name == "asdkj":
+            await channel.delete()
 bot.run(token)
